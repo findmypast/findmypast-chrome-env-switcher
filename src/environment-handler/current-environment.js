d03600b4d70dcf45a163f7fb9a8935cdfe466ceb
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-03-26 16:47:34 +0000

    The tests pass

--- src/environment-handler/current-environment.js
+++ src/environment-handler/current-environment.js
@@ -1,3 +1,13 @@
-const currentEnvironment = () => {};
+const { ENVIRONMENT } = require("./consts");
+
+const currentEnvironment = url => {
+  if (url.includes("local")) {
+    return ENVIRONMENT.LOCAL;
+  } else if (url.includes("integration")) {
+    return ENVIRONMENT.INTEGRATION;
+  } else {
+    return ENVIRONMENT.PRODUCTION;
+  }
+};
 
 module.exports = currentEnvironment;
