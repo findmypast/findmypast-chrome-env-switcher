e15edd512f863013961827254ff0a54c71a88c05
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-03-31 09:03:21 +0100

    Now highlights the correct env

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,34 @@
-let changeColor = document.getElementById("changeColor");
+import currentEnvironment from "./environment-handler/current-environment.js";
+import CONSTANTS from "./environment-handler/consts.js";
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "' + color + '";'
-    });
-  });
-};
+chrome.tabs.getSelected(tab => {
+  const currentEnv = currentEnvironment(tab.url);
 
-chrome.storage.sync.get("color", function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute("value", data.color);
+  const productionButton = document.getElementById("production");
+  const integrationButton = document.getElementById("integration");
+  const localButton = document.getElementById("local");
+
+  if (currentEnv === CONSTANTS.ENVIRONMENT.LOCAL) {
+    localButton.classList.toggle("active");
+  } else if (currentEnv === CONSTANTS.ENVIRONMENT.INTEGRATION) {
+    integrationButton.classList.toggle("active");
+  } else if (currentEnv === CONSTANTS.ENVIRONMENT.PRODUCTION) {
+    productionButton.classList.toggle("active");
+  }
 });
+
+// let changeColor = document.getElementById("changeColor");
+
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//     chrome.tabs.executeScript(tabs[0].id, {
+//       code: 'document.body.style.backgroundColor = "' + color + '";'
+//     });
+//   });
+// };
+
+// chrome.storage.sync.get("color", function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute("value", data.color);
+// });
