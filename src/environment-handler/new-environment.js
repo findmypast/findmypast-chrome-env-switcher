550ccda7321420c771985e57386008816e37ab6e
Cécile Journeaux <cjourneaux@findmypast.com>
2020-05-13 16:44:39 +0100

    Removing unused code

--- src/environment-handler/new-environment.js
+++ src/environment-handler/new-environment.js
@@ -3,19 +3,21 @@ import CONSTANTS from "./consts";
 import getUrlProperties from './get-url-properties';
 import writeNewUrl from './write-new-url';
 
-const newEnvironment = (fromEnv, toEnv, url) => {
-  const fromURL = new URL(url);
+const newEnvironment = (targetEnv, currentUrl) => {
+  const url = new URL(currentUrl);
 
-  const pathname = fromURL.pathname;
-  const searchParams = fromURL.search;
+  const pathname = url.pathname;
+  const searchParams = url.search;
 
   const {
     isSearchPage,
     topLevelDomain,
-  } = getUrlProperties(fromURL.origin)
+  } = getUrlProperties(url.origin);
+  // TODO should the responsibility for the path lie here 
+  // or in getUrlProperties
 
   return writeNewUrl({
-    environment: toEnv,
+    environment: targetEnv,
     isSearchPage,
     topLevelDomain,
     pathname,
--- src/popup.js
+++ src/popup.js
@@ -1,33 +1,30 @@
-import currentEnvironment from "./environment-handler/current-environment.js";
 import newEnvironment from "./environment-handler/new-environment.js";
 
 import { ENVIRONMENT } from "./environment-handler/consts.js";
 
 const button = (env) => document.getElementById(env);
 
-const buttonClick = (from, to, tab) => () => {
-  const newUrl = newEnvironment(from, to, tab.url);
-
+const buttonClick = (target, tab) => () => {
+  const newUrl = newEnvironment(target, tab.url);
   chrome.tabs.update(tab.id, { url: newUrl });
 };
 
 chrome.tabs.getSelected(tab => {
-  const currentEnv = currentEnvironment(tab.url);
+  const {
+    environment: currentEnv
+  } = getUrlProperties(tab.url);
 
   button(currentEnv).classList.toggle("active");
 
   button(ENVIRONMENT.LOCAL).onclick = buttonClick(
-    currentEnv,
     ENVIRONMENT.LOCAL,
     tab
   );
   button(ENVIRONMENT.INTEGRATION).onclick = buttonClick(
-    currentEnv,
     ENVIRONMENT.INTEGRATION,
     tab
   );
   button(ENVIRONMENT.PRODUCTION).onclick = buttonClick(
-    currentEnv,
     ENVIRONMENT.PRODUCTION,
     tab
   );
